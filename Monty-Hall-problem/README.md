63da1092b772e02ba9ceb990e339d03398c71f65
Alissa Cheng <alissa.cheng@protonmail.ch>
2020-11-17 12:07:30 +0100

    Add files via upload

---
+++ Monty-Hall-problem/README.md
@@ -0,0 +1,24 @@
+# Monty Hall problem
+## The Monty Hall game
+
+## The probabilities
+Below is a tree diagram to demonstrate the probability of winning if the contestant switches door choice.
+
+![](https://github.com/mehfluffy/Monty-Hall-problem/blob/main/tree_switch.svg)
+
+We can also count the probability of "winning by switching" by listing all possibilities in a table instead:
+
+| Prize door | First choice | Monty opens | Switch | Don't switch |
+| - | - | - | - | - |
+| 1 | 1 | 2 or 3 | L | W |
+| 1 | 2 | 3 | W | L |
+| 1 | 3 | 2 | W | L |
+| 2 | 1 | 3 | W | L |
+| 2 | 2 | 1 or 3 | L | W |
+| 2 | 3 | 1 | W | L |
+| 3 | 1 | 2 | W | L |
+| 3 | 2 | 1 | W | L |
+| 3 | 3 | 1 or 2 | L | W |
+| . | . | **W/total =** | **2/3** | **1/3** |
+
+For the table, since the "door numbers" are arbitrary, it would have sufficed to list only the case for `prize door = 1`, which yields the same probabilities overall. Because there is only a $1/3$ chance that the first choice aligns with the prize door, there is a $1/3$ chance that one could win by _not_ switching. The probability of winning by switching is then $1-1/3 = 2/3$, and not $1/3 \times 1/2$ as intuition might suggest, because the second choice is far from random. Since Monty must open a door with no prize, the original probability $2/3$ of winning via the two unchosen doors "condenses" towards the door that Monty did _not_ open, because the probability that "the unchosen door $x$ has a prize" becomes $0$ when it has been opened by Monty; while the originally chosen door retains its probability to have the prize of $1/3$ because Monty's choices did not entail the original door.
---
+++ Monty-Hall-problem/monty_hall.py
@@ -0,0 +1,38 @@
+n_arr = np.logspace(1, 4, 4, dtype=np.int32)
+
+def game():
+    door = [1, 2, 3]
+    prize_door = np.random.randint(1, len(door)+1, dtype='int')
+    chosen_door = np.random.randint(1, len(door)+1, dtype='int')
+    if prize_door == chosen_door:   # Monty chooses randomly to open one of the 2 possible doors
+        p = np.where(door == prize_door, 0, 0.5)
+        opened_door = np.random.choice(door, p=p)
+    else:   # Monty only has 1 possible door to open
+        p = np.where(np.logical_or(door == prize_door, door == chosen_door), 0, 1)
+        opened_door = np.random.choice(door, p=p)
+    door.remove(opened_door)   # This is when the remaining 2/3 condenses 
+    second_door = np.random.choice(door)   # Assuming that the contestant chooses randomly still
+    switch = True
+    win = 0
+    if second_door == chosen_door: 
+        switch = False
+    if second_door == prize_door: 
+        win = 1
+    return switch, win
+
+for N in n_arr:
+    print("N =", N)
+    switch_arr = np.empty((0))
+    stay_arr = np.empty((0))
+    for i in range(N):
+        switch, win = game()
+        if switch == True: 
+            switch_arr = np.append(switch_arr, win)
+        else: 
+            stay_arr = np.append(stay_arr, win)
+    switch_win_prob = switch_arr.sum() / len(switch_arr)   # win is either 0 or 1, so taking the sum works
+    stay_win_prob = stay_arr.sum() / len(stay_arr)         # len() is how many times switching (or not) was done
+    print("P(Prize if Switch) =", switch_win_prob)
+    print("P(Prize if no Switch) =", stay_win_prob)
+    print("Ratio Switch/noSwitch =", switch_win_prob/stay_win_prob)   # should tend to 2 in long run, (2/3)/(1/3) = 2
+    print()
